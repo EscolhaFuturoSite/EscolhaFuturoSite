5b4dd6fe55161db9702c8318998f7a348186a21c
Gabriel Lima <glimaf1601@gmail.com>
2022-02-07 11:37:26 -0300

    first deploy 07/02

--- pages/index.js
+++ pages/index.js
@@ -190,7 +190,7 @@ export default function mainPage(){
             </Image>
         )
     }
-    function Footer(props){
+    function Footer(){
         const contactList = [
             "FaInstagram",
             "FaWhatsapp",
